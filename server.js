c996b70abc91614f1e20cb7baf5c62f3ec2b8692
Julien <ju.collet@gmail.com>
2017-12-25 16:52:28 +0100

    add path resolve

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + 'client/index.html');
+  res.sendFile(path.resolve(__dirname + 'client/index.html'));
 });
 
 var server = app.listen(80, function () {
